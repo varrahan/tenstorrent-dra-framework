8617d3b8c0f73d3be90affb6595e1671939a78fb
varrahan <varrahan@gmail.com>
2026-08-03 14:28:34 -0400

    Move tests back to src/test directory

---
+++ src/test/cli_test.go
@@ -0,0 +1,17 @@
+package test
+
+import (
+	"os"
+	"os/exec"
+	"strings"
+	"testing"
+)
+
+func TestNodeCommandRejectsNonPositiveIntervalBeforeClusterSetup(t *testing.T) {
+	command := exec.Command("go", "run", "../cmd/tt-dra-driver", "node", "--interval=0s")
+	command.Env = append(os.Environ(), "NODE_NAME=worker-a")
+	output, err := command.CombinedOutput()
+	if err == nil || !strings.Contains(string(output), "inventory interval must be positive") {
+		t.Fatalf("command error = %v, output = %s", err, output)
+	}
+}
--- src/test/lifecycle_test.go
+++ src/test/lifecycle_test.go
@@ -1,4 +1,4 @@
-package lifecycle_test
+package test
 
 import (
 	"context"
